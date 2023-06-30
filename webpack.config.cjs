3b9ec92e7dd70984d1038ac2bf9da36284e3abd2
Alex Khitry <khitrii03@gmail.com>
2023-06-30 12:12:10 +0300

    init: create repository

---
+++ webpack.config.cjs
@@ -0,0 +1,75 @@
+const path = require('path')
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+              test: /\.s[ac]ss$/,
+              exclude: /node_modules/,
+              use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
+        alias: {
+          '@src': path.resolve(__dirname, 'src'),
+          '@assets': path.resolve(__dirname, 'src/assets'),
+          '@components': path.resolve(__dirname, 'src/components'),
+          '@contexts': path.resolve(__dirname, 'src/contexts'),
+          '@hooks': path.resolve(__dirname, 'src/hooks'),
+          '@layouts': path.resolve(__dirname, 'src/layouts'),
+          '@pages': path.resolve(__dirname, 'src/pages'),
+          '@routes': path.resolve(__dirname, 'src/routes'),
+          '@services': path.resolve(__dirname, 'src/services'),
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './index.html'
+        }),
+        new CopyPlugin({
+          patterns: [
+            { from: './src/assets', to: '' },
+          ],
+        }),
+        new Dotenv({
+          systemvars: true
+        }),
+        new MiniCssExtractPlugin(),
+      ],
+    optimization: {
+        minimizer: [
+          new TerserPlugin(),
+          new CssMinimizerPlugin(),
+        ],
+    },
+    devServer: {
+        static: {
+          directory: path.resolve(__dirname, './dist')
+        }
+    }
+}
