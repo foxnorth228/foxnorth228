e49d41c4b21c4eb339a9845d50cee0342ad9aece
Alex Khitry <khitrii03@gmail.com>
2023-07-01 11:13:47 +0300

    refactor: change scss based on creating scale function

--- src/components/authorFace/authorFace.tsx
+++ src/components/authorFace/authorFace.tsx
@@ -3,10 +3,10 @@ import './authorFace.scss';
 
 const AuthorFace = () => {
   return (
-    <div className='authorFace'>
-      <div className='authorFace__image'></div>
-      <h2 className='authorFace__name'>Alexander Khitry</h2>
-      <h3 className='authorFace__job'>Frontend developer</h3>
+    <div className="authorFace">
+      <div className="authorFace__image"></div>
+      <h2 className="authorFace__name">Alexander Khitry</h2>
+      <h3 className="authorFace__job">Frontend developer</h3>
     </div>
   );
 };
--- src/layouts/info-block/info-block.tsx
+++ src/layouts/info-block/info-block.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './info-block.scss';
 import AuthorFace from '@components/authorFace/authorFace';
+import BasicInfoBlock from '@components/basic-info-block/basic-info-block';
 
 const InfoBlock = () => {
   return (
     <section className="basicInfo">
       <AuthorFace />
+      <BasicInfoBlock />
     </section>
   );
 };
