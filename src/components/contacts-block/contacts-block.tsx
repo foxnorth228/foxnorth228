6f5db6738149d84b531c0bc2f284cc6cd7f62339
Alex Khitry <khitrii03@gmail.com>
2023-07-03 14:52:27 +0300

    feat: add ru to contacts

--- src/components/contacts-block/contacts-block.tsx
+++ src/components/contacts-block/contacts-block.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './contacts-block.scss';
 import InfoBlock from '../info-block/info-block';
+import useLanguage from '@src/hooks/useLanguage';
 
 const ContactsBlock = () => {
+  const locale = useLanguage('contacts');
   const figures = [
     ['./contacts/github.svg', 'Github', 'https://github.com/foxnorth228/'],
     ['./contacts/linkedin.svg', 'Linkedin', 'https://www.linkedin.com/in/alex-khitry-016687265'],
     ['./contacts/telegram.svg', 'Telegram', 'https://t.me/foxnorth228'],
     ['./contacts/whatsapp.svg', 'WhatsApp', 'https://wa.me/375292677541'],
     ['./contacts/skype.svg', 'Skype', 'skype:cid.bd609d3ff0e2494c?userinfo'],
-    ['./contacts/link.svg', 'My portfolio', 'https://master--moonlit-sprite-20c9ef.netlify.app/'],
+    ['./contacts/link.svg', locale.titlePort, 'https://master--moonlit-sprite-20c9ef.netlify.app/'],
   ];
   return (
-    <InfoBlock title="Contacts" titleSize="contactsBlock__title" className="contactsBlock">
+    <InfoBlock
+      title={locale.titleContacts}
+      titleSize="contactsBlock__title"
+      className="contactsBlock"
+    >
       <div className="contactsBlock__elems">
         {figures.map((el, i) => (
           <a key={i} href={el[2]} className="contactsBlock__link">
