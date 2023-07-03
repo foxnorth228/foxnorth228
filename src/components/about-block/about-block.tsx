8c6054159b217a5b23a41d4e0ea1c9f2ec5ad347
Alex Khitry <khitrii03@gmail.com>
2023-07-03 15:02:35 +0300

    feat: add ru to about-block

--- src/components/about-block/about-block.tsx
+++ src/components/about-block/about-block.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import './about-block.scss';
 import InfoBlock from '../info-block/info-block';
+import useLanguage from '@src/hooks/useLanguage';
 
 const AboutBlock = () => {
+  const locale = useLanguage('aboutme');
   return (
-    <InfoBlock title="About me" titleSize="aboutBlock__title" className="aboutBlock">
-      <p>
-        {
-          "Hi! I am Alexander Khitry, 20 yo, a frontend developer (React.js).  \
-          I am a fourth-year student in BSUIR, my specialization is connected with computer science. \
-          When I was 18 yo I tried myself in creating sites for the first time and was very excited about it. \
-          I worked a lot to dig in frontend development and finished some courses. \
-          I didn't stop there and continue learning something new. \
-          I enjoy creating websites according to all modern quality standards. \
-          My typical stack is React, Typescript, Sass, Webpack. \
-          In current time I haven't got work experience in this sphere, but I am working on some pet projects to hone my skills"
-        }
-      </p>
+    <InfoBlock title={locale.titleAbout} titleSize="aboutBlock__title" className="aboutBlock">
+      <p>{locale.text}</p>
     </InfoBlock>
   );
 };
