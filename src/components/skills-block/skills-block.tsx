625a414cb57e1dc5853e10cbf94f8d8f32bdac00
Alex Khitry <khitrii03@gmail.com>
2023-07-03 14:25:48 +0300

    feat: add ru to skills-block

--- src/components/skills-block/skills-block.tsx
+++ src/components/skills-block/skills-block.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './skills-block.scss';
 import InfoBlock from '../info-block/info-block';
+import useLanguage from '@src/hooks/useLanguage';
 
 const SkillsBlock = () => {
+  const locale = useLanguage('skills');
   return (
-    <InfoBlock title="Skills" titleSize="skillsBlock__title" className="skillsBlock">
+    <InfoBlock
+      title={locale.titleSkillsBlock}
+      titleSize="skillsBlock__title"
+      className="skillsBlock"
+    >
       <ul className="skillsBlock__list">
         <li className="skillsBlock__elem">HTML/CSS</li>
         <li>JavaScript</li>
