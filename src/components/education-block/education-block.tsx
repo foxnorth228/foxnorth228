2df4d8dd3de34865e91aa3e0f4e5043ab609a9fc
Alex Khitry <khitrii03@gmail.com>
2023-07-03 15:08:24 +0300

    feat: add ru to education-block

--- src/components/education-block/education-block.tsx
+++ src/components/education-block/education-block.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './education-block.scss';
 import InfoBlock from '../info-block/info-block';
+import useLanguage from '@src/hooks/useLanguage';
 
 const EducationBlock = () => {
+  const locale = useLanguage('education');
   return (
-    <InfoBlock title="Education" titleSize="educationBlock__title" className="educationBlock">
+    <InfoBlock
+      title={locale.titleEducBlock}
+      titleSize="educationBlock__title"
+      className="educationBlock"
+    >
       <div className="educationBlock__elem">
         <p className="educationBlock__year">2020-2025</p>
         <div className="educationBlock__desc">
-          <p className="educationBlock__univer">
-            Belarusian State University of Informatics and Radioelectronics (BSUIR)
-          </p>
-          <p className="educationBlock__educType">Incomplete higher education</p>
-          <p className="educationBlock__spec">Software engineer</p>
+          <p className="educationBlock__univer">{locale.univer1}</p>
+          <p className="educationBlock__educType">{locale.educType1}</p>
+          <p className="educationBlock__spec">{locale.spec1}</p>
         </div>
       </div>
     </InfoBlock>
